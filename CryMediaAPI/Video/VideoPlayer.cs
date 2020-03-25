a492aca35c61755d2ed14d7a2724b962134ecbcd
Adam Veselič <adam.veselic@hotmail.com>
2020-03-25 14:31:26 +0100

    Video player now supports direct writing of frames into the player. README adjusted.

--- CryMediaAPI/Video/VideoPlayer.cs
+++ CryMediaAPI/Video/VideoPlayer.cs
@@ -2,18 +2,27 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace CryMediaAPI.Video
 {
-    public class VideoPlayer
+    public class VideoPlayer : IDisposable
     {
         string ffplay;
+        Stream input;
+        Process ffplayp;
+        bool outOpened = false;
+
         public string Filename { get; }
 
-        public VideoPlayer(string input, string ffplayExecutable = "ffplay")
+        /// <summary>
+        /// Used for playing video data
+        /// </summary>
+        /// <param name="input">Input video to play (can be left empty if planning on playing frames directly)</param>
+        /// <param name="ffplayExecutable">Name or path to the ffplay executable</param> 
+        public VideoPlayer(string input = null, string ffplayExecutable = "ffplay")
         {
             ffplay = ffplayExecutable;
 
@@ -25,6 +34,7 @@ namespace CryMediaAPI.Video
         /// </summary>
         public void Play()
         {
+            if (string.IsNullOrEmpty(Filename)) throw new InvalidOperationException("No filename was specified!");
             FFmpegWrapper.RunCommand(ffplay, $"-i \"{Filename}\"");
         }
 
@@ -33,8 +43,65 @@ namespace CryMediaAPI.Video
         /// </summary>
         public Process PlayInBackground()
         {
+            if (string.IsNullOrEmpty(Filename)) throw new InvalidOperationException("No filename was specified!");
             FFmpegWrapper.OpenOutput(ffplay, $"-i \"{Filename}\"", out Process p);
             return p;
+        }
+
+        /// <summary>
+        /// Open player for writing frames for playing.
+        /// </summary>
+        /// <param name="width">Video frame width</param>
+        /// <param name="height">Video frame height</param>
+        /// <param name="framerateFrequency">Video framerate (frequency form)</param>
+        /// <param name="showFFplayOutput">Show FFplay output for debugging purposes.</param>
+        public void OpenWrite(int width, int height, string framerateFrequency, bool showFFplayOutput = false)
+        {
+            if (outOpened) throw new InvalidOperationException("Player is already opened for writing frames!");
+
+            input = FFmpegWrapper.OpenInput(ffplay, $"-f rawvideo -video_size {width}:{height} -framerate {framerateFrequency} -pixel_format rgb24 -i -",
+                out ffplayp, showFFplayOutput);
+
+            outOpened = true;
+        }
+
+        /// <summary>
+        /// Close player for writing frames.
+        /// </summary>
+        public void CloseWrite()
+        {
+            if (!outOpened) throw new InvalidOperationException("Player is not opened for writing frames!");
+
+            try
+            {
+                try
+                {
+                    if (!ffplayp.HasExited) ffplayp.Kill();
+                }
+                catch { }
+
+                input.Dispose();              
+            }
+            finally
+            {
+                outOpened = false;
+            }         
+        }
+
+        /// <summary>
+        /// Write frame to FFplay to play it.
+        /// </summary>
+        /// <param name="frame">Video frame to write to player</param>
+        public void WriteFrame(VideoFrame frame)
+        {
+            if (!outOpened) throw new InvalidOperationException("Player not opened for writing frames!");
+
+            input.Write(frame.RawData.Span);
+        }
+
+        public void Dispose()
+        {
+            if (outOpened) CloseWrite();
         }
     }
 }
--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using CryMediaAPI.Video;
 
-namespace TestingConsole    
+namespace TestingConsole
 {
     class Program
     {
@@ -13,9 +14,9 @@ namespace TestingConsole
             string input = args[0];
             string output = args[1];
 
-            
             // ReadWriteAudio(input, output);
             // ReadWriteVideo(input, output);
+            ReadPlayVideo(input, output);
         }
 
         static void ReadWriteAudio(string input, string output)
@@ -31,7 +32,7 @@ namespace TestingConsole
 
             using (var writer = new VideoWriter(output, video.Metadata.Width, video.Metadata.Height, video.Metadata.AvgFramerate))
             {
-                writer.OpenForWriting(false);
+                writer.OpenWrite(false);
 
                 var frame = new VideoFrame(video.Metadata.Width, video.Metadata.Height);
                 while (true)
@@ -51,6 +52,38 @@ namespace TestingConsole
                         }
 
                     writer.WriteFrame(frame);
+                }
+            }
+        }
+
+        static void ReadPlayVideo(string input, string output)
+        {
+            var video = new VideoReader(input);
+            video.LoadMetadata().Wait();
+            video.Load();
+
+            using (var player = new VideoPlayer(output))
+            {
+                player.OpenWrite(video.Metadata.Width, video.Metadata.Height, video.Metadata.AvgFramerateText, true);
+
+                var frame = new VideoFrame(video.Metadata.Width, video.Metadata.Height);
+                while (true)
+                {
+                    // read next frame
+                    var f = video.NextFrame(frame);
+                    if (f == null) break;
+
+
+                    for (int i = 0; i < 100; i++)
+                        for (int j = 0; j < 100; j++)
+                        {
+                            var px = frame.GetPixels(i, j).Span;
+                            px[0] = 255;
+                            px[1] = 0;
+                            px[2] = 0;
+                        }
+
+                    player.WriteFrame(frame);
                 }
             }
         }
