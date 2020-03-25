42620b5dff0b9c94f8e91ef76c5ad96ff861a351
CryShana <cryshana@cryshana.me>
2020-03-25 14:00:30 +0100

    Added basic Video player. Modified FFmpeg wrapper.

---
+++ CryMediaAPI/Video/VideoPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CryMediaAPI.Video
+{
+    public class VideoPlayer
+    {
+        string ffplay;
+        public string Filename { get; }
+
+        public VideoPlayer(string input, string ffplayExecutable = "ffplay")
+        {
+            ffplay = ffplayExecutable;
+
+            Filename = input;
+        }
+
+        /// <summary>
+        /// Play video
+        /// </summary>
+        public void Play()
+        {
+            FFmpegWrapper.RunCommand(ffplay, $"-i \"{Filename}\"");
+        }
+    }
+}
