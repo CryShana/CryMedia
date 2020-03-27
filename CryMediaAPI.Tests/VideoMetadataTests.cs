9e1a844e6c08531c2dabda0237905644612f494a
Adam Veselič <adam.veselic@hotmail.com>
2020-03-27 10:08:52 +0100

    Added empty tests project

---
+++ CryMediaAPI.Tests/VideoMetadataTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CryMediaAPI.Tests
+{
+    public class VideoMetadataTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
