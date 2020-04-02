bad76fa5b3b2effc4d593e774954bf890c167fc4
CryShana <cryshana@cryshana.me>
2020-04-02 13:35:45 +0200

    added metadata tests and some sample files

---
+++ CryMediaAPI.Tests/Resources/Resources.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace CryMediaAPI.Resources
+{
+    public static class Res 
+    {
+        public const string Directory = "Resources";
+            
+        public const string Video_Mp4 = "small.mp4";
+        public const string Video_Webm = "small.webm";
+        public const string Video_Flv = "small.flv";
+        public const string Audio_Mp3 = "horse.mp3";
+        public const string Audio_Ogg = "horse.ogg";
+
+        public static string GetPath(string resourceName) => Path.Combine(Directory, resourceName);
+    }
+}
--- CryMediaAPI.Tests/VideoMetadataTests.cs
+++ CryMediaAPI.Tests/VideoMetadataTests.cs
@@ -1,14 +1,65 @@
-using System;
 using Xunit;
+using System;
+using CryMediaAPI.Video;
+using CryMediaAPI.Resources;
+using System.Threading.Tasks;
 
 namespace CryMediaAPI.Tests
 {
     public class VideoMetadataTests
     {
         [Fact]
-        public void Test1()
+        public async Task LoadMetadataMp4()  
         {
+            var video = new VideoReader(Res.GetPath(Res.Video_Mp4));
 
+            await video.LoadMetadata();
+
+            Assert.True(video.Metadata.Codec == "h264");
+            Assert.True(video.Metadata.AvgFramerate == 30);
+            Assert.True(video.Metadata.AvgFramerateText == "30/1");
+            Assert.True(Math.Abs(video.Metadata.Duration - 5.533333) < 0.01);
+            Assert.True(video.Metadata.Width == 560);
+            Assert.True(video.Metadata.Height == 320);
+            Assert.True(video.Metadata.BitDepth == 8);
+            Assert.True(video.Metadata.BitRate == 465641);
+            Assert.True(video.Metadata.PixelFormat == "yuv420p");
+            Assert.True(video.Metadata.Streams.Length == 2);
+        }
+
+        [Fact]
+        public async Task LoadMetadataWebm()
+        {
+            var video = new VideoReader(Res.GetPath(Res.Video_Webm));
+
+            await video.LoadMetadata();
+
+            Assert.True(video.Metadata.Codec == "vp8");
+            Assert.True(video.Metadata.AvgFramerate == 30);
+            Assert.True(video.Metadata.AvgFramerateText == "30/1");
+            Assert.True(Math.Abs(video.Metadata.Duration - 5.568) < 0.01); 
+            Assert.True(video.Metadata.Width == 560);
+            Assert.True(video.Metadata.Height == 320);
+            Assert.True(video.Metadata.PixelFormat == "yuv420p");
+            Assert.True(video.Metadata.Streams.Length == 2);  
+        }
+
+        [Fact]
+        public async Task LoadMetadataFlv()
+        {
+            var video = new VideoReader(Res.GetPath(Res.Video_Flv));
+
+            await video.LoadMetadata();
+
+            Assert.True(video.Metadata.Codec == "flv1");
+            Assert.True(video.Metadata.AvgFramerate == 25);
+            Assert.True(video.Metadata.AvgFramerateText == "25/1");
+            Assert.True(Math.Abs(video.Metadata.Duration - 5.56) < 0.01);
+            Assert.True(video.Metadata.Width == 320); 
+            Assert.True(video.Metadata.Height == 240); 
+            Assert.True(video.Metadata.BitRate == 800000);
+            Assert.True(video.Metadata.PixelFormat == "yuv420p");
+            Assert.True(video.Metadata.Streams.Length == 2); 
         }
     }
 }
