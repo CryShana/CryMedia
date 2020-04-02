62928a7373015af2453bdab6ee3ee36837d74e37
CryShana <cryshana@cryshana.me>
2020-04-02 11:13:46 +0200

    added ffmpeg methods for getting encoders, decoders and formats

--- CryMediaAPI/FFmpegWrapper.cs
+++ CryMediaAPI/FFmpegWrapper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace CryMediaAPI
 {
@@ -9,6 +11,9 @@ namespace CryMediaAPI
     /// </summary>
     public static class FFmpegWrapper
     {
+        static readonly Regex CodecRegex = new Regex(@"(?<type>[VAS\.])[F\.][S\.][X\.][B\.][D\.] (?<codec>[a-zA-Z0-9_-]+)\W+(?<description>.*)\n?", RegexOptions.Compiled);
+        static readonly Regex FormatRegex = new Regex(@"(?<type>[D\s][E\s]) (?<format>[a-zA-Z0-9_\-,]+)\W+(?<description>.*)\n?", RegexOptions.Compiled);
+
         /// <summary>
         /// Run given command (arguments) using the given executable name or path
         /// </summary>
@@ -100,5 +105,57 @@ namespace CryMediaAPI
         }
         public static (Stream input, Stream output) Open(string executable, string command, bool showOutput = false)
             => Open(executable, command, out _, showOutput);
+
+        public static Dictionary<string, (string Description, MediaType Type)> GetEncoders(string ffmpegExecutable = "ffmpeg")
+        {
+            var data = new Dictionary<string, (string Description, MediaType Type)>();
+            var r = RunCommand(ffmpegExecutable, "-encoders -v quiet", true);
+            var mtc = CodecRegex.Matches(r.output);
+            foreach (Match m in mtc)
+            {
+                char t = m.Groups["type"].Value[0];
+                data.Add(m.Groups["codec"].Value, (m.Groups["description"].Value, t == 'A' ? MediaType.Audio : (t == 'V' ? MediaType.Video : MediaType.Subtitle)));
+            }
+            return data;
+        }
+        public static Dictionary<string, (string Description, MediaType Type)> GetDecoders(string ffmpegExecutable = "ffmpeg")
+        {
+            var data = new Dictionary<string, (string Description, MediaType Type)>();
+            var r = RunCommand(ffmpegExecutable, "-decoders -v quiet", true);
+            var mtc = CodecRegex.Matches(r.output);
+            foreach (Match m in mtc)
+            {
+                char t = m.Groups["type"].Value[0];
+                data.Add(m.Groups["codec"].Value, (m.Groups["description"].Value, t == 'A' ? MediaType.Audio : (t == 'V' ? MediaType.Video : MediaType.Subtitle)));
+            }
+            return data;
+        }
+        public static Dictionary<string, (string Description, MuxingSupport Support)> GetFormats(string ffmpegExecutable = "ffmpeg")
+        {
+            var data = new Dictionary<string, (string Description, MuxingSupport Support)>();
+            var r = RunCommand(ffmpegExecutable, "-formats -v quiet", true);
+            var mtc = FormatRegex.Matches(r.output);
+            foreach (Match m in mtc)
+            {
+                string t = m.Groups["type"].Value.Trim();
+                data.Add(m.Groups["format"].Value, (m.Groups["description"].Value, 
+                    t == "DE" ? MuxingSupport.MuxDemux : (t == "D" ? MuxingSupport.Demux : MuxingSupport.Mux)));
+            }
+            return data;
+        }
+
+        public enum MediaType
+        {
+            Video,
+            Audio,
+            Subtitle
+        }
+
+        public enum MuxingSupport
+        {
+            MuxDemux,
+            Mux,
+            Demux
+        }
     }
 }
